5d3203884955d3f7c614483f0a5d25df48b6b0da
dessinger <juliancorrea1403@gmail.com>
2022-10-26 03:25:55 -0300

    test java primera parte seccion 3

---
+++ holla.java
@@ -0,0 +1,18 @@
+
+    public class Main {
+
+
+            public static int main(String[] args){
+                int resultado ;
+
+                resultado = suma( 4 , 2 , 2);
+
+                System.out.println(resultado);
+            }
+        
+        
+        public static int suma(int a, int b, int c) {
+            return a + b + c;
+        }
+
+  }
